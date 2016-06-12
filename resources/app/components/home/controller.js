3a7f44f7ba8f692d3d1f1295b6498236e278f5ec
David Iruzubieta <david.iruzubieta@gmail.com>
2016-06-12 04:23:15 +0200

    Login and logout functionality

--- resources/app/components/home/controller.js
+++ resources/app/components/home/controller.js
@@ -4,13 +4,20 @@
         .module('smartmarket-web')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', '$rootScope'];
+    HomeController.$inject = ['$scope', '$rootScope', 'API', '$state'];
     /**
      * TodoController
      * @param $scope
+     * @param $rootScope
+     * @param API
+     * @param $state
      * @constructor
      */
-    function HomeController($scope, $rootScope) {
+    function HomeController($scope, $rootScope, API, $state) {
+        $scope.logout = logout;
 
+        function logout() {
+            $state.go("login");
+        }
     }
 })();
